10c0b333eb7b5c0b5612d2ccd048f82fb1f72d3a
paniz nourani <paniznourani80@gmail.com>
2024-02-02 21:38:50 +0330

    the new packages added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
+
+// import {axios} from 'axios'
 function App() {
   return (
-    <div>
+    <div className="w-full h-full relative">
+      <div className="text-center p-4">
+        <input type="text" className="py-3 px-6 w-[700px] text-lg rounded-3xl border border-gray-200 text-gray-600 placeholder:text-gray-400" />
+      </div>
       <h1>weather app</h1>
     </div>
   );
