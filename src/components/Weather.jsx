472ac3e585aac6df199740c54203e3a0828c8113
paniz nourani <paniznourani80@gmail.com>
2024-02-03 01:42:01 +0330

    collecting more data from weather API and displaying it on the weather card

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -11,8 +11,7 @@ const Weather = ({ weatherData }) => {
               <div className="flex flex-col items-start justify-between h-full">
                 <div>
                   <p className="text-xl">
-                    {weatherData.name},
-                    {weatherData.sys.country}
+                    {weatherData.name},{weatherData.sys.country}
                   </p>
                   <p className="text-sm">
                     {weatherData.weather[0].description}
@@ -20,15 +19,52 @@ const Weather = ({ weatherData }) => {
                 </div>
                 <div>
                   <h1 className="text-6xl font-semibold">
-                    {weatherData.main.temp.toFixed()}°C
+                    {weatherData.main.temp.toFixed()} °C
                   </h1>
                 </div>
               </div>
             </div>
-           
+            <div className="w-1/2 flex flex-col justify-between items-end">
+              <div className="relative">
+                <img
+                  src={`http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`}
+                  alt=""
+                  className="w-[120px]"
+                />
+              </div>
+              {/* </div> */}
+            {weatherData.name !== undefined ? (
+                <div className="flex flex-col justify-evenly gap-y-2 my-4 mx-auto text-xs ">
+                <div className="flex justify-between gap-x-8">
+                  <p>Feels Like</p>
+                  <p className="font-bold w-20">
+                    {weatherData.main.feels_like.toFixed()} °C
+                  </p>
+                  </div>
+                <div className="flex justify-between gap-x-8">
+                  <p>Humidity</p>
+                  <p className="font-bold w-20">
+                    {weatherData.main.humidity.toFixed()} %
+                  </p>
+                </div>
+                <div className="flex justify-between gap-x-8">
+                  <p>Wind Speed</p>
+                  <p className="font-bold w-20">
+                    {weatherData.wind.speed.toFixed()} KPH
+                  </p>
+                </div>
+                <div className="flex justify-between gap-x-8">
+                  <p>Pressure</p>
+                  <p className="font-bold w-20">
+                    {weatherData.main.pressure} hPa
+                  </p>
+                </div>
+              </div>
+            ) : null}
+            </div>
           </div>
         </div>
-      ) : null}
+      ) : <h1 className="flex justify-center font-bold text-4xl my-5">Search a Location</h1>}
     </div>
   );
 };
